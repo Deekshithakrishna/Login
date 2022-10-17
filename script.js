dbb67ea5c6c523173102f67106426738f41c6343
Spexzee <abub5979@gmail.com>
2022-10-17 14:23:45 +0530

    Start

---
+++ script.js
@@ -0,0 +1,11 @@
+let forg = document.getElementById('forget');
+forg.addEventListener('click', function run() {
+    alert("check ur email");
+})
+let btn = document.getElementById('button');
+btn.addEventListener('click', function run() {
+    alert("LogIn Success!!!!")
+
+})
+
+
